c46d7555ee47f3d905313c44f50861e45ba063d6
Wilson Angelie Tan <wilsonangelietann@gmail.com>
2025-11-28 18:57:17 +0700

    refactor: refactor api route for fetch court

--- src/app/api/reservations/route.ts
+++ src/app/api/reservations/route.ts
@@ -2,32 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@/lib/auth/auth-client";
 import { CourtService } from "@/lib/services/court.service";
 
-export async function GET(req: NextRequest) {
-  const { data: session } = await getSession();
+export async function POST(req: NextRequest) {
 
-  if (!session?.user.id) {
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
-  }
-
-  try{
-    // later implement DTO if have time
-    const courts = await CourtService.getAllCourt();
-
-    return NextResponse.json(
-      {
-        success: true,
-        data: courts
-      },
-      { status: 200 }
-    );
-
-  } catch(err) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: "Internal Server Error",
-      },
-      { status: 500 }
-    );
-  }
 }
