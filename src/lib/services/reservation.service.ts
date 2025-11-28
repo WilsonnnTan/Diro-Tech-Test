939e841683a275f6f50cbdcef4436fac58147d6e
Wilson Angelie Tan <wilsonangelietann@gmail.com>
2025-11-28 19:05:32 +0700

    refactor: add UTC +7 converter

--- src/lib/services/reservation.service.ts
+++ src/lib/services/reservation.service.ts
@@ -1,15 +1,32 @@
 import { ReservationRepository } from "../repositories/reservation.repo";
 import { ReservationDTO, ReservationStatusEnum } from "../dto/reservation.schema";
 
+function convertToUTCPlus7(date: Date | string): Date {
+  const d = new Date(date);
+  // add 7 hours to UTC
+  const utc7 = new Date(d.getTime() + 7 * 60 * 60 * 1000);
+  return utc7;
+}
+
 export const ReservationService = {
 	async getReservationDetail(userId: string, reservationId: string){
 		const res = await ReservationRepository.findReservationDetail(userId, reservationId);
+
+		  if (res && res.date) {
+      	res.date = convertToUTCPlus7(res.date);
+    	}
+
 		return res;
 	},
 
 	async getAllReservationByUserId(userId: string) {
-		const res = await ReservationRepository.findAllReservationByUserId(userId);
-		return res;
+		const resList = await ReservationRepository.findAllReservationByUserId(userId);
+    return resList.map(res => {
+      if (res.date) {
+        res.date = convertToUTCPlus7(res.date);
+      }
+      return res;
+    });
 	},
 
 }
